666d8ad95f3dcd25343b11eb7065c902af8d9b09
Sean Mulligan <sean.paul.mulligan@gmail.com>
2021-02-14 22:32:28 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,17 +13,9 @@ end# This one works! dont change!
 
 def find_the_cheese(array)# code an argument here
    #["cheddar", "gouda", "camembert"]
-#array.any? do |array| 
-  array.include?("chedder")
-  if true puts "chedder"
-else puts "nil"
- end
- end
-#{|cheese|  cheese.include?(cheese_types)}
-#array of strings
-#look through these strings to find
-#and return the first string that is a type of cheese
-#["cheddar", "gouda", and "camembert"]
+array.find {|array|array.include?("chedder")}
+
+
 
 
 
